e6906a5b0f7368da77625604c54ddb7e344d54c2
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-11-24 14:14:05 +0330

    Tracked

--- Disconnected Entities/Program.cs
+++ Disconnected Entities/Program.cs
@@ -1,12 +1,20 @@
 ﻿
 using Disconnected_Entities.Models;
 
-var context = new AppDb();
-var person = new Person("Mehrshad", "Khaki");
-context.Persons.Add(person);    
-context.SaveChanges();
-Console.WriteLine("succeeded");
 
+// entities states with respect to tracking: (Tracked, Detached, Disconnected)
+
+var context = new AppDb();
+//var person = new Person("Mehrshad", "Khaki");
+//context.Persons.Add(person);    
+//context.SaveChanges();
+//Console.WriteLine("succeeded");
+
+var person = context.Persons.First();
+person.Name = "Ahmad";
+context.SaveChanges();
+Console.WriteLine($" state is : {context.Entry(person).State}");
+Console.WriteLine("succeeded");
 
 
 Console.ReadKey();
